c57044d642a3e5cd8aa9f6f3cafc256b36329514
allenki_akshay <allenkiakshay8322@gmail.com>
2024-01-05 19:16:12 +0530

    update

---
+++ prisma/migrations/20240105134420_dev/migration.sql
@@ -0,0 +1,60 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "logindata" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateTable
+CREATE TABLE "createteam" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "teamid" TEXT NOT NULL,
+    "teamname" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "createmember" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "teamid" TEXT NOT NULL,
+    "teamname" TEXT NOT NULL,
+    "memid" TEXT NOT NULL,
+    "memname" TEXT NOT NULL,
+    "mememail" TEXT NOT NULL,
+    "memphone" TEXT NOT NULL,
+    "memrole" TEXT NOT NULL,
+    "isverfied" BOOLEAN NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "memberverify" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "mememail" TEXT NOT NULL,
+    "creatememid" TEXT NOT NULL,
+    "teamid" TEXT NOT NULL,
+    "teamname" TEXT NOT NULL,
+    "randomcode" INTEGER NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "createteam_teamid_key" ON "createteam"("teamid");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "memberverify_creatememid_key" ON "memberverify"("creatememid");
